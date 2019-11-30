d99be8d8accb143659c0982bee65da61965b3773
Erwan Lemonnier <erwan@lemonnier.se>
2019-11-30 23:48:30 +0100

    Add README to pypi description

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ An extension of pymacaron seamlessly adding asynchronous serialisation to and fr
 DynamoDB.
 
 See more at [pymacaron.com](http://pymacaron.com/db.html)
+
+## Author
+
+Erwan Lemonnier<br/>
+[github.com/pymacaron](https://github.com/pymacaron)</br>
+[github.com/erwan-lemonnier](https://github.com/erwan-lemonnier)<br/>
+[www.linkedin.com/in/erwan-lemonnier/](https://www.linkedin.com/in/erwan-lemonnier/)
--- setup.py
+++ setup.py
@@ -29,6 +29,12 @@ if version:
     version = version.strip()
 print("version: %s" % version)
 
+# read the contents of the README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='pymacaron-dynamodb',
     version=version,
@@ -37,6 +43,8 @@ setup(
     author='Erwan Lemonnier',
     author_email='erwan@lemonnier.se',
     description='Serialize/Deserialize Swagger JSON objects to and from DynamoDB, with transactional writes',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=[
         'pymacaron-unit',
         'pymacaron',
